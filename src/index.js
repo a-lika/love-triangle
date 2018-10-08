9bad14489efeb1b12e134a1016ac37da082f6083
Anzhelika <anzhelika110k@gmail.com>
2018-10-08 17:25:39 +0300

    refactor: replace var to const

--- src/index.js
+++ src/index.js
@@ -3,13 +3,12 @@
  * @returns number of love triangles
  */
 module.exports = function getLoveTrianglesCount(preferences = []) {
-  // your implementation
-  var cont=0;
-  preferences.unshift('');
-  for(var i=1; i<preferences.length; i++){
-  if (preferences[preferences[preferences[i]]] == i){
-  cont++;
-  }
-  }
-  return Math.floor(cont/3);
+    let count = 0;
+    preferences.unshift('');
+    for (let i = 0; i < preferences.length; i++) {
+        if (preferences[preferences[preferences[i + 1]]] === i + 1) {
+            count++;
+        }
+    }
+    return Math.floor(count / 3);
 };
